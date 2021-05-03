7960a1fc7a5541b3bf22b34dd31c16539dc4438e
Fernando-Braulio <fernandobraulio@outlook.com.br>
2021-05-02 21:52:35 -0300

    Login com Firebase

---
+++ android/app/src/main/kotlin/br/com/fernandobraulio/login_firebase/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.fernandobraulio.login_firebase
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/ui/android/login_page.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: ListView(
+          shrinkWrap: true,
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          children: [
+            Hero(
+              tag: 'hero',
+              child: CircleAvatar(
+                backgroundColor: Colors.transparent,
+                radius: 48.0,
+                child: Image.asset("assets/logo.png"),
+              ),
+            ),
+            SizedBox(height: 48.0),
+            TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              autofocus: false,
+              // initialValue: "emailteste@gmail.com",
+              decoration: InputDecoration(
+                hintText: "E-mail: ",
+                contentPadding:
+                    EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(32.0),
+                ),
+              ),
+            ),
+            SizedBox(height: 8.0),
+            TextFormField(
+              obscureText: true,
+              autofocus: false,
+              decoration: InputDecoration(
+                hintText: "Sua Senha:",
+                contentPadding:
+                    EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(32.0),
+                ),
+              ),
+            ),
+            SizedBox(height: 24.0),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 16.0),
+              child: RaisedButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(24.0)),
+                onPressed: () {},
+                padding: EdgeInsets.all(12.0),
+                color: Get.theme.primaryColor,
+                child: Text(
+                  "Logar",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+            FlatButton(
+              onPressed: () {},
+              child: Text(
+                "Esqueceu a senha?",
+                style: TextStyle(color: Colors.black54),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
